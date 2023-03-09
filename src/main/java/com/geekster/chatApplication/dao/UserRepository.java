ef897d87ef4d3ff918d84ff913d46d1b0be89643
sankalptrimade <sankalptrimade0014@gmail.com>
2023-03-09 19:02:33 +0530

    first commit

---
+++ src/main/java/com/geekster/chatApplication/dao/UserRepository.java
@@ -0,0 +1,34 @@
+package com.geekster.chatApplication.dao;
+
+import com.geekster.chatApplication.model.Users;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<Users, Integer> {
+
+    @Query(value = "Select * from tbl_user where username = :username and status_id = 1", nativeQuery = true)
+    public List<Users> findByUsername(String username);
+
+    @Query(value = "select * from tbl_user where user_id = :userId and status_id = 1", nativeQuery = true)
+    public List<Users> getUserByUserId(int userId);
+
+    @Query(value = "select * from tbl_user where status_id = 1", nativeQuery = true)
+    public List<Users> getAllUsers();
+
+    //This query is for having version 3.0.3
+    @Modifying
+    @Transactional
+    @Query(value = "update tbl_user set status_id = 2 where user_id = :userId",
+            countQuery = "Select count(*) from tbl_user", nativeQuery = true)
+    public void deleteUserByUserId(int userId);
+
+    //This query is for not having version 3.0.3
+//    @Query(value = "update tbl_user set status_id = 2 where user_id = :userId", nativeQuery = true)
+//    public void deleteUserByUserId(int userId);
+
+}
